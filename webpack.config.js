5b79afcad2d20370814c206de0cfae5c7a1cd977
Dakshesh Bijawar <bdaksheshbijawar735@gmail.com>
2025-07-16 02:37:12 +0530

    complete

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,28 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: './src/main.js',
-    mode: 'development',
-    output: {
-      path: path.resolve(__dirname, './dist'),
-      filename: 'main.js',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html', 
-            favicon: './src/assets/favicon.svg',
-        }), 
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.(sa|sc|c)ss$/,
-                use: ["style-loader", "css-loader"]
-            }, 
-            {
-                test: /\.(jpe?g|png|gif|svg|mp4)$/i,
-                type: "asset",
-              }, 
-        ],
+  entry: "./src/main.js",
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "main.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: ["style-loader", "css-loader"],
       },
-  };
+      {
+        test: /\.(jpe?g|png|gif|svg|mp4)$/i,
+        type: "asset",
+      },
+    ],
+  },
+};
